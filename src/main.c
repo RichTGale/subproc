be5e17371472ea547b34d18bb9cf92181e6543f5
richtgale <richtgale@gmail.com>
2022-10-14 22:25:33 +1100

    Added comments

--- src/main.c
+++ src/main.c
@@ -17,36 +17,47 @@
 #include "mycutils.h"
 #include "subproc.h"
 
+#define SUBPROC_RUN_SECS ( uint64_t ) 10
+#define BLOCK_EXEC_FREQ ( uint64_t ) 1
+
 int main (int argc, char* argv[])
 {
-    subproc sp;
-    struct timespec ts_sp;
+    subproc sp; // The sub-process.
+    struct timespec ts_sp; // timespec for timing the sub-process.
+    // This timespec times the execution of a code block that runs
+    // while the sub-process is running.
     struct timespec ts_out;
-    bool running = true;
+    bool running = true; // Whether the loop should loop.
 
+    // Initialising the subprocess and using it to execute a
+    // shell command.
     subproc_init( &sp );
     subproc_exec( &sp, "ls" );
+
+    // Setting the timespecs with the current time.
     start_timer( &ts_sp );
     start_timer( &ts_out );
 
+    // Running the processes.
     while ( running )
     {
-        if ( !( check_timer( ts_sp, NANOS_PER_SEC * ( uint64_t) 10 ) ) )
+        if ( check_timer( ts_sp, NANOS_PER_SEC * SUBPROC_RUN_SECS ) )
         {
-            if ( check_timer( ts_out, NANOS_PER_SEC * ( uint64_t) 1 ) )
-            {
-                fprintf( stdout, 
-                        "[ %s ] Sub-process is running...\n", timestamp() );
-                start_timer( &ts_out );
-            }
-        }
-        else
-        {
+            // The sub-process has run for its specified period of time
+            // so here we are terminating it.
             fprintf( stdout, "[ %s ] Sub-process is being terminated...\n", 
                     timestamp() );
             subproc_term( &sp );
             subproc_free( &sp );
             running = false;
+        }
+        else if ( check_timer( ts_out, NANOS_PER_SEC * BLOCK_EXEC_FREQ ) )
+        {
+            // This block is run once per the specified number of seconds
+            // while the child process is running.
+	        fprintf( stdout, 
+                    "[ %s ] Sub-process is running...\n", timestamp() );
+	        start_timer( &ts_out ); // Restarting the timer. 
         }
     }
 
