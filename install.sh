02b270a1358f1b8d960e3d80abd93a504561db69
richtgale <richtgale@gmail.com>
2022-10-14 21:44:11 +1100

    Removed ccls

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cmake -H. -B ./ -DCMAKE_BUILD_TYPE=debug -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
+cmake -H. -B ./ -DCMAKE_BUILD_TYPE=debug -DCMAKE_EXPORT_COMPILE_COMMANDS=NO
 
 mkdir ./output
